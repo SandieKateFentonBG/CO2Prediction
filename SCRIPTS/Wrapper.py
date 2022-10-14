dea2d1b26d70b699110735621b5cc82415b0efbc
sfenton <sfenton@bollinger-grohmann.fr>
2022-10-14 18:58:32 +0200

    changed wrapper to a class

--- SCRIPTS/Wrapper.py
+++ SCRIPTS/Wrapper.py
@@ -1,216 +1,180 @@
 from sklearn.feature_selection import RFECV
 from sklearn.feature_selection import RFE
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import KFold
-from sklearn.model_selection import LeaveOneOut, StratifiedKFold
-from sklearn.linear_model import LinearRegression
-import pandas as pd
+# from sklearn.model_selection import LeaveOneOut
+
+#DEFAULT VALUES
 
 rs = 42
-RFEEstimators = {'LinearRegression': LinearRegression(),
-                 'DecisionTreeRegressor': DecisionTreeRegressor(random_state = rs),
-                  'RandomForestRegressor' : RandomForestRegressor(random_state = rs),
-                  'GradientBoostingRegressor' : GradientBoostingRegressor(random_state = rs),
-                  'DecisionTreeClassifier' : DecisionTreeClassifier(random_state = rs)}
-
-step = 1 #features removed at every itteration
-scoring="r2"
-n_features_to_select = 10
-cv = KFold(n_splits=5, shuffle=True, random_state=rs) #cross-validation splitting - ensures same split for every function run
-loo = LeaveOneOut() #alternative to cv TODO : check this
-featureCount = [5, 10, 15]
+n_features_to_select = 15
+featureCount = [5, 10, 15, 20, 25]
 
 """
 Questions
 1. Trainscore is too high - What should i evaluate my rfe on? validation set? > what does the RFECV use as a score? 
 2. Scaling, When should it be done? Look into pipeline
 3. I inserted a random-state to have stable results - is this ok?
-4. leave one out?
+4. loo = LeaveOneOut() #alternative to cv - use this? 
 """
 
-def RFECVGridsearch(RFEEstimators, xTrain, yTrain, step, cv, scoring , display = False, testTuple = None):
-    """
+class WrapFeatures:
 
+    def __init__(self, method, estimator, formatedDf, yLabel, n_features_to_select, featureCount, step= 1,
+                 cv = KFold(n_splits=5, shuffle=True, random_state=42), scoring="r2"):
 
-    Look optimal number of features
-    (or, the best combination of n features, given their importance for the wrapped estimator)
+        # step - features removed at every iteration
+        # KFold - cross-validation splitting - ensures same split for every function run
 
-    :param RFEEstimators:
-    :param xTrain: Input samples - array-like of shape (n_samples, n_features) (xTrain.to_numpy())
-    :param yTrain: Target values - array-like of shape (n_samples,) (yTrain.to_numpy()) or (n_samples, n_outputs) (yTrain.to_numpy().ravel())
-    :param step:
-    :param cv:
-    :param scoring:
-    :param display:
-    :param testTuple: (xTest, yTest) - insert these if you want to test rfe on testing, else None
-    :return:
-    """
+        self.method = method
+        self.estimator = estimator #unfit estimator
+        self.RFElimination(formatedDf, n_features_to_select, yLabel)
+        self.RFEliminationCV(formatedDf, step, cv, scoring, yLabel)
+        self.RFEHyperparameterSearch(formatedDf, featureCount)
 
-    rfecvDict = dict()
+        # self.n_features_to_select
+        # self.rfe/rfeCV/rfehyp
+            # self.rfe_droppedLabels
+            # self.rfe_selectedLabels
+            # self.rfe_trainScore
+            # self.rfe_valScore
 
-    for k,estimator in RFEEstimators.items():
+            # self.rfe_trainDf
+            # self.rfe_valDf
+            # self.rfe_testDf
+            # self.rfe_XTrain
+            # self.rfe_XVal
+            # self.rfe_XTest
+            # self.rfe_yTrain
+            # self.rfe_yVal
+            # self.rfe_yTest
 
-        rfecvDict[k] = dict()
+            # self.rfeHyp_featureCount
+            # self.rfeHyp_trainScore
+            # self.rfeHyp_valScore
+
+    def RFElimination(self, formatedDf, n_features_to_select, yLabel):
+
+        """
+        Look for n most important features
+        (or, the best combination of n features, given their importance for the wrapped estimator)
+        """
+        self.n_features_to_select = n_features_to_select
+        rfe = RFE(self.estimator, n_features_to_select=self.n_features_to_select)
+
+        self.rfe = rfe.fit(formatedDf.XTrain.to_numpy(), formatedDf.yTrain.to_numpy().ravel())
+        self.rfe_selectedLabels = formatedDf.XTrain.columns[rfe.support_]
+        self.rfe_trainScore = self.rfe.score(formatedDf.XTrain.to_numpy(), formatedDf.yTrain.to_numpy().ravel())
+        self.rfe_valScore = self.rfe.score(formatedDf.XVal.to_numpy(), formatedDf.yVal.to_numpy().ravel())
+
+        self.rfe_droppedLabels = [label for label in formatedDf.XTrain.columns if label not in self.rfe_selectedLabels]
+
+        self.rfe_trainDf = formatedDf.trainDf.drop(columns=self.rfe_droppedLabels)
+        self.rfe_valDf = formatedDf.valDf.drop(columns=self.rfe_droppedLabels)
+        self.rfe_testDf = formatedDf.testDf.drop(columns=self.rfe_droppedLabels)
+        self.rfe_XTrain =self.rfe_trainDf.drop(columns=yLabel)
+        self.rfe_XVal = self.rfe_valDf.drop(columns=yLabel)
+        self.rfe_XTest = self.rfe_testDf.drop(columns=yLabel)
+        self.rfe_yTrain = self.rfe_trainDf[yLabel]
+        self.rfe_yVal = self.rfe_valDf[yLabel]
+        self.rfe_yTest = self.rfe_testDf[yLabel]
+
+    def RFEDisplay(self):
+
+        print("RFE with:", self.method)
+        print("Number of features fixed:", self.n_features_to_select)
+        print("Score on training", self.rfe_trainScore)
+        print('Selected feature labels', list(self.rfe_selectedLabels))
+        print("Score on validation", self.rfe_valScore)
+        print("")
+
+    def RFEliminationCV(self, formatedDf, step, cv, scoring, yLabel):
+
+        """    Look optimal number of features
+        (or, the best combination of n features, given their importance for the wrapped estimator)"""
 
         # TODO : pipeline - insert scaling here?
-        rfecv = RFECV(estimator=estimator, step=step, cv=cv, scoring=scoring)
-        rfecv = rfecv.fit(xTrain.to_numpy(), yTrain.to_numpy().ravel())
+        rfecv = RFECV(estimator=self.estimator, step=step, cv=cv, scoring=scoring)
+        self.rfecv = rfecv.fit(formatedDf.XTrain.to_numpy(), formatedDf.yTrain.to_numpy().ravel())
+        self.rfecv_trainScore = self.rfecv.score(formatedDf.XTrain.to_numpy(), formatedDf.yTrain.to_numpy().ravel())
+        self.rfecv_valScore = self.rfecv.score(formatedDf.XVal.to_numpy(), formatedDf.yVal.to_numpy().ravel())
+        self.rfecv_selectedLabels = formatedDf.XTrain.columns[self.rfecv.support_]
 
-        rfecvDict[k]['model'] = rfecv
+        self.rfecv_droppedLabels = [label for label in formatedDf.XTrain.columns if label not in self.rfecv_selectedLabels]
 
-        if testTuple:
-            xTest, yTest = testTuple
-            score = rfecv.score(xTest.to_numpy(), yTest.to_numpy().ravel())
-            rfecvDict[k]['Test Score'] = score
+        self.rfecv_trainDf = formatedDf.trainDf.drop(columns=self.rfecv_droppedLabels)
+        self.rfecv_valDf = formatedDf.valDf.drop(columns=self.rfecv_droppedLabels)
+        self.rfecv_testDf = formatedDf.testDf.drop(columns=self.rfecv_droppedLabels)
+        self.rfecv_XTrain =self.rfecv_trainDf.drop(columns=yLabel)
+        self.rfecv_XVal = self.rfecv_valDf.drop(columns=yLabel)
+        self.rfecv_XTest = self.rfecv_testDf.drop(columns=yLabel)
+        self.rfecv_yTrain = self.rfecv_trainDf[yLabel]
+        self.rfecv_yVal = self.rfecv_valDf[yLabel]
+        self.rfecv_yTest = self.rfecv_testDf[yLabel]
 
-        if display :
-            print("RFECV Gridsearch:")
-            print("Estimator:", k)
-            print("Number of features:", rfecv.n_features_)
-            # score function reduces x to selected features and returns score
-            print("CV Score on training", rfecv.score(xTrain.to_numpy(), yTrain.to_numpy().ravel()))
-            #print('Selected feature labels', xTrain.columns[rfecv.support_])
-            if testTuple:
-                print("Score on testing", score)
-            print("")
+    def RFEHyperparameterSearch(self, formatedDf, featureCount):
 
-    """
-    
-    EXAMPLE : 
-    
-    rfecvDict = { DecisionTreeRegressor : {'model': rfecv, 'Test Score' : score} }
-    
-    """
+        """Look for number of features - by selecting the n most important features
+        (or, the best combination of n features, given their importance for the wrapped estimator)"""
 
-    return rfecvDict
+        self.rfeHyp_featureCount = featureCount
+        trainScoreList = []
+        testScoreList = []
 
-def RFEGridsearch(RFEEstimators,n_features_to_select, xTrain, yTrain, display = False, testTuple = None) :
+        for f in featureCount:
 
-    """
-    Look for n most important features
-    (or, the best combination of n features, given their importance for the wrapped estimator)
-    """
+            rfeHyp = RFE(self.estimator, n_features_to_select=f)
+            rfeHyp = rfeHyp.fit(formatedDf.XTrain.to_numpy(), formatedDf.yTrain.to_numpy().ravel())
 
-    rfeDict = dict()
+            trainScoreList.append(rfeHyp.score(formatedDf.XTrain.to_numpy(), formatedDf.yTrain.to_numpy().ravel()))
+            testScoreList.append(rfeHyp.score(formatedDf.XVal.to_numpy(), formatedDf.yVal.to_numpy().ravel()))
 
-    for k,estimator in RFEEstimators.items():
+        self.rfeHyp_trainScore = trainScoreList
+        self.rfeHyp_valScore= testScoreList
 
-        rfeDict[k] = dict()
-        rfe = RFE(estimator, n_features_to_select=n_features_to_select)
-        rfe = rfe.fit(xTrain.to_numpy(), yTrain.to_numpy().ravel())
+    def RFECVDisplay(self):
 
-        rfeDict[k]['model'] = rfe
+        print("RFECV with:", self.method)
+        print("Number of features from CV:", self.rfecv.n_features_)
+        print("Score on training", self.rfecv_trainScore)
+        print('Selected feature labels', list(self.rfecv_selectedLabels))
+        print("Score on validation", self.rfecv_valScore)
+        print("")
 
-        if testTuple:
-            xTest, yTest = testTuple
-            score = rfe.score(xTest.to_numpy(), yTest.to_numpy().ravel())
-            # rfeDict_Test[k] = score
-            rfeDict[k]['Test Score'] = score
+    def RFEHypSearchDisplay(self):
 
-        if display :
-            print("RFE Gridsearch:")
-            print("Estimator:", k)
-            print("Number of features:", n_features_to_select)
-            print("Score on training", rfe.score(xTrain.to_numpy(), yTrain.to_numpy().ravel()))
-            print('Selected feature labels', xTrain.columns[rfe.support_])
-            if testTuple:
-                print("Score on testing", score)
-            print("")
+        print("RFE Param Search with:", self.method)
+        print("Number of features compared", self.rfeHyp_featureCount)
+        print("Score on training", self.rfeHyp_trainScore)
+        print("Score on validation", self.rfeHyp_valScore)
+        print("")
 
-    """
+    def check(self):
 
-    EXAMPLE : 
+        print((self.rfe_trainDf).shape)
+        print((self.rfe_valDf).shape)
+        print((self.rfe_testDf).shape)
 
-    rfeDict = { DecisionTreeRegressor : {'model': rfe, 'Test Score' : score} }
+        print((self.rfe_XTrain).shape)
+        print((self.rfe_XVal).shape)
+        print((self.rfe_XTest).shape)
+        print((self.rfe_yTrain).shape)
+        print((self.rfe_yVal).shape)
+        print((self.rfe_yTest).shape)
 
-    """
+        print((self.rfecv_trainDf).shape)
+        print((self.rfecv_valDf).shape)
+        print((self.rfecv_testDf).shape)
 
-    return rfeDict
+        print((self.rfecv_XTrain).shape)
+        print((self.rfecv_XVal).shape)
+        print((self.rfecv_XTest).shape)
+        print((self.rfecv_yTrain).shape)
+        print((self.rfecv_yVal).shape)
+        print((self.rfecv_yTest).shape)
 
-def RFEHyperparameterSearch(RFEEstimators,featureCount, xTrain, yTrain, display = False, testTuple = None):
-
-    """
-    Look for number of features - by selecting the n most important features
-    (or, the best combination of n features, given their importance for the wrapped estimator)
-    :param RFEEstimators:
-    :param featureCount:
-    :param xTrain:
-    :param yTrain:
-    :param display:
-    :param testTuple:
-    :return:
-    """
-    paramDict = dict()
-
-    for k in RFEEstimators.keys():
-
-        paramDict[k] = dict()
-        paramDict[k]['featureCount'] = featureCount  # [featureList, trainScore, testScore]
-        paramDict[k]['Train Score'] = []
-        paramDict[k]['Test Score'] = []
-
-    for f in featureCount:
-        rfeDict = RFEGridsearch(RFEEstimators, f, xTrain, yTrain, testTuple=testTuple)
-
-        for k in RFEEstimators.keys():
-            paramDict[k]['Train Score'].append(rfeDict[k]['model'].score(xTrain.to_numpy(), yTrain.to_numpy().ravel()))
-            if testTuple:
-                paramDict[k]['Test Score'].append(rfeDict[k]['Test Score'])
-    if display:
-        for k, v in paramDict.items():
-            print(k)
-            print(v)
-
-    """
-
-    EXAMPLE : 
-
-    paramDict = { DecisionTreeRegressor : {'featureCount': rfe, 'Train Score': score , 'Test Score' : score}}
-
-    """
-
-
-    return paramDict
-
-def WrapperLabels(rfeDict):
-    RFELabelsDict = dict()
-    for k in rfeDict.keys():
-        RFELabelsDict[k] = rfeDict[k]['model'].support_
-    return RFELabelsDict
-
-def labelsToDrop(allLabels, labelsToKeep):
-
-    labelsToDrop = []
-    for label in allLabels:
-        if label not in labelsToKeep:
-            labelsToDrop.append(label)
-
-    return labelsToDrop
-
-def EliminateDf(xtrainDf, xvalidDf, xtestDf, ytrainDf, yvalidDf, ytestDf, rfeDict):
-
-    RFEDf = dict()
-    for k in rfeDict.keys():
-
-        RFETrainDf = xtrainDf.columns[rfeDict[k]['model'].support_]
-        RFEValidDf = xvalidDf.columns[rfeDict[k]['model'].support_]
-        RFETestDf = xtestDf.columns[rfeDict[k]['model'].support_]
-
-        #todo : fix train df concatenated from xtrain and y train
-
-        # RFETrainDf = pd.concat([RFETrainDf, ytrainDf], axis=1)
-        # RFEValidDf = pd.concat([RFEValidDf, yvalidDf], axis=1)
-        # RFETestDf = pd.concat([RFETestDf, ytestDf], axis=1)
-        print('here')
-        print(k)
-        print(RFETrainDf)
-
-        RFEDf[k] = [RFETrainDf, RFEValidDf, RFETestDf]
-
-    return RFEDf
+        print(self.rfeHyp_featureCount)
+        print(self.rfeHyp_trainScore)
+        print(self.rfeHyp_valScore)
 
 
 # https://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.RFE.html
--- SCRIPTS/base.py
+++ SCRIPTS/base.py
@@ -63,19 +63,10 @@ Dashboard Input :
 
 myFormatedDf = formatedDf(learningDf, xQuantLabels, xQualLabels, yLabels, yUnitFactor = processingParams['yUnit'])
 
-trainDf, valDf, testDf = myFormatedDf.trainDf, myFormatedDf.valDf, myFormatedDf.testDf
 
 print("train", type(myFormatedDf.trainDf), myFormatedDf.trainDf.shape)
 print("validate", type(myFormatedDf.valDf), myFormatedDf.valDf.shape)
 print("test", type(myFormatedDf.testDf), myFormatedDf.testDf.shape)
-
-yTrain = myFormatedDf.yTrain
-xTrain = myFormatedDf.XTrain
-yVal = myFormatedDf.yVal
-xVal = myFormatedDf.XVal
-yTest = myFormatedDf.yTest
-xTest = myFormatedDf.XTest
-
 
 """
 ------------------------------------------------------------------------------------------------------------------------
@@ -86,62 +77,50 @@ xTest = myFormatedDf.XTest
 """
 FILTER - SPEARMAN
 """
-myFilteredDf = FilterFeatures(trainDf, valDf, testDf, baseLabels = xQuantLabels, yLabel = yLabels[0])
+spearmanFilter = FilterFeatures(myFormatedDf.trainDf, myFormatedDf.valDf, myFormatedDf.testDf, baseLabels = xQuantLabels, yLabel = yLabels[0])
 
 print('')
 print('FILTER - SPEARMAN CORRELATION')
-print('LABELS : ', myFilteredDf.trainDf.shape, type(myFilteredDf.trainDf))
-print(myFilteredDf.selectedLabels)
+print('LABELS : ', spearmanFilter.trainDf.shape, type(spearmanFilter.trainDf))
+print(spearmanFilter.selectedLabels)
 
 # plotCorrelation(myFilteredDf.correlationMatrix_All, DBpath, displayParams, filteringName="nofilter")
 # plotCorrelation(myFilteredDf.correlationMatrix_NoUncorrelated, DBpath, displayParams, filteringName="dropuncorr")
 # plotCorrelation(myFilteredDf.correlationMatrix_NoRedundant, DBpath, displayParams, filteringName="dropcolinear")
 
-# """
-# ELIMINATE - RFE
-# """
-#
-# rfecvDict = RFECVGridsearch(RFEEstimators, xTrain, yTrain, step, cv, scoring, display = False, testTuple = (xVal, yVal))
-# print('rfecvDict', rfecvDict)
-#
-# rfeDict = RFEGridsearch(RFEEstimators,n_features_to_select = 15, xTrain = xTrain, yTrain = yTrain, display = False,
-#                         testTuple = (xVal, yVal))
-#
-# paramDict = RFEHyperparameterSearch(RFEEstimators,featureCount = featureCount, xTrain = xTrain, yTrain = yTrain,
-#                                     display = False, testTuple = (xVal, yVal))
-# # print('paramDict',paramDict)
-# #todo : check summary equation table
-# #todo : check formats - numpy vs panda / ravel()/ reshape(-1,1),...
-# #todo : add linear regression
-# #todo : how to evaluate RFE - the goal is not to perform the best prediction - what scoring should be inserted?
-# #todo : understand fit vs fit transform > make sure i am working with updated data
-#
-# RFEDict = EliminateDf(xTrain, xVal, xTest, yTrain, yVal, yTest, rfeDict)
-#
-# RFELabelsDict = WrapperLabels(rfeDict)
-#
-# RFElabelsToDrop = labelsToDrop(allLabels, labelsToKeep)
-#
-# RFECVDict = EliminateDf(xTrain, xValid, xTest, yTrain, yValid, yTest, rfecvDict)
-#
-# [RFETrainDict, RFEValidDict, RFETestDict] = RFEDict['RandomForestRegressor']
-# print('RFETrainDict', RFETrainDict)
-#
-# RFECVTrainDict, RFECVValidDict, RFECVTestDict = RFEDict['RandomForestRegressor']
-# print(RFECVTrainDict)
-#
-# #todo : fix train df concatenated from xtrain and y train - see wrapper - eliminate
-# print('')
-# print('ELIMINATE - RECURSIVE FEATURE ELIMINATION')
-# print('RandomForestRegressor')
-# print('LABELS : ', list(RFETrainDict.columns.values))
-# print(list(RFETrainDict.columns.values))
-#
-# print('')
-# print('ELIMINATE - RECURSIVE FEATURE ELIMINATION - CROSS VALIDATED')
-# print('RandomForestRegressor')
-# print('LABELS : ', list(RFECVTrainDict.columns.values))
-# print(list(RFECVTrainDict.columns.values))
+"""
+ELIMINATE - RFE
+"""
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
+LR_RFE = WrapFeatures(method = 'LinearRegression', estimator = LinearRegression(), formatedDf = myFormatedDf,
+                         yLabel = yLabels[0], n_features_to_select = n_features_to_select, featureCount = featureCount)
+RFR_RFE = WrapFeatures(method = 'RandomForestRegressor', estimator = RandomForestRegressor(random_state = rs),
+                        formatedDf = myFormatedDf, yLabel = yLabels[0], n_features_to_select = n_features_to_select, featureCount = featureCount)
+DTR_RFE = WrapFeatures(method = 'DecisionTreeRegressor', estimator = DecisionTreeRegressor(random_state = rs),
+                        formatedDf = myFormatedDf, yLabel = yLabels[0], n_features_to_select = n_features_to_select, featureCount = featureCount)
+GBR_RFE = WrapFeatures(method = 'GradientBoostingRegressor', estimator = GradientBoostingRegressor(random_state = rs),
+                        formatedDf = myFormatedDf, yLabel = yLabels[0], n_features_to_select = n_features_to_select, featureCount = featureCount)
+DTC_RFE = WrapFeatures(method = 'DecisionTreeClassifier', estimator = DecisionTreeClassifier(random_state = rs),
+                        formatedDf = myFormatedDf, yLabel = yLabels[0], n_features_to_select = n_features_to_select, featureCount = featureCount)
+
+print('ELIMINATE - RECURSIVE FEATURE ELIMINATION')
+
+for _RFE in [LR_RFE, RFR_RFE, DTR_RFE, GBR_RFE, DTC_RFE]:
+    _RFE.RFEDisplay()
+    _RFE.RFECVDisplay()
+    _RFE.RFEHypSearchDisplay()
+
+#todo : check summary equation table
+#todo : check formats - numpy vs panda / ravel()/ reshape(-1,1),...
+#todo : add linear regression
+#todo : how to evaluate RFE - the goal is not to perform the best prediction - what scoring should be inserted?
+#todo : understand fit vs fit transform > make sure i am working with updated data
+
 
 """
 ------------------------------------------------------------------------------------------------------------------------
@@ -153,10 +132,7 @@ print(myFilteredDf.selectedLabels)
 Hyperparam Search
 """
 
-# yTrain = filteredTrainDf[yLabels].to_numpy()
-# xTrain = filteredTrainDf.drop(columns=yLabels).to_numpy()
-# print('xTrain', xTrain.shape, xTrain)
-# print('yTrain', yTrain.shape, yTrain)
+
 # model = predictors[4]
 # print(model)
 # for k,v in model.items():
