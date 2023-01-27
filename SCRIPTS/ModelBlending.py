added various blender constructors / saving options - fixed average computation

SCRIPTS/ModelBlending.py
@@ -41,7 +41,6 @@ class BlendModel:
 
             predictor = model.Estimator
             learningDf = model.learningDf
-            print(learningDf.XVal.shape, learningDf.XTrain.shape, learningDf.XTest.shape)
 
             if Val:
                 rawXTrain, rawyTrain = learningDf.XVal.to_numpy(), learningDf.yVal.to_numpy().ravel()
@@ -63,16 +62,18 @@ class BlendModel:
         self.blendXtrain = pd.concat(blend_train_sets, axis=1) #dim 400*i #naming different because second order data
         self.blendXtest = pd.concat(blend_test_sets, axis=1) #dim 20*i
 
-        # building the final model using the meta features
+        # building the final model using the meta features # this should be done by a cv of 5 folds on the training set
         if Gridsearch:
             njobs = os.cpu_count() - 1
             grid = GridSearchCV(self.modelPredictor, param_grid=self.param_dict, scoring=self.scoring, refit=self.refit,
                                 n_jobs=njobs, return_train_score=True)
             grid.fit(self.blendXtrain, self.yTrain)
+            self.Param = grid.best_params_
             self.Estimator = grid.best_estimator_
 
         else :
             self.Estimator = self.modelPredictor.fit(self.blendXtrain, self.yTrain)
+            self.Param = None
 
         self.yPred = self.Estimator.predict(self.blendXtest)
 
@@ -86,7 +87,26 @@ class BlendModel:
         self.ResidMean = round(np.mean(np.abs(self.Resid)),2) #round(np.mean(self.Resid),2)
         self.ResidVariance = round(np.var(self.Resid),2)
 
-        self.ModelWeights = self.Estimator.coef_
+        if hasattr(self.Estimator, 'coef_'): # LR, RIDGE, ELASTICNET, KRR Kernel Linear, SVR Kernel Linear
+            self.isLinear = True
+            content = self.Estimator.coef_
+            if type(content[0]) == np.ndarray:
+                content = content[0]
+
+        elif hasattr(self.Estimator, 'dual_coef_'): #KRR
+            self.isLinear = True
+            content = self.Estimator.dual_coef_
+            if type(content[0]) == np.ndarray:
+                content = content[0]
+
+        else :
+            self.isLinear = False
+            content = 'Estimator is non linear - no weights can be querried'
+        weights = [round(num, self.rounding) for num in list(content)]
+        print('weights', len(weights), weights)
+        self.ModelWeights = weights
+
+        # self.Weights = self.Estimator.coef_ #todo : this naming was changed from ModelWeights = self.Estimator.coef_
 
 def sortedModels(GS_FSs, NBestScore ='TestR2'): #'TestAcc' #todo : the score was changed from TestAcc to TestR2
     #sorting key = 'TestAcc' , last in list
@@ -151,7 +171,7 @@ def selectnBestModels(GS_FSs, sortedModelsData, n=10, checkR2 = True):
     return nBestModels
 
 
-def reportGS_Scores_Blending(blendModel, displayParams, DBpath, blendingScore):
+def reportGS_Scores_Blending(blendModel, displayParams, DBpath, NBestScore, NCount):
 
 
     if displayParams['archive']:
@@ -167,13 +187,13 @@ def reportGS_Scores_Blending(blendModel, displayParams, DBpath, blendingScore):
         BlendingDf = pd.DataFrame(columns=columns, index=index)
         for col in columns[:-1]:
             BlendingDf[col] = [model.__getattribute__(col) for model in blendModel.modelList] + [blendModel.__getattribute__(col)]
-        BlendingDf['ModelWeights'] = [round(elem,3) for elem in list(blendModel.ModelWeights)] + [0]
+        BlendingDf['Weights'] = [round(elem,3) for elem in list(blendModel.ModelWeights)] + [0] #todo : this naming was changed from ModelWeights
         sortedDf = BlendingDf.sort_values('ModelWeights', ascending=False)
 
         AllDfs = [BlendingDf, sortedDf]
         sheetNames = ['Residuals_MeanVar', 'Sorted_Residuals_MeanVar']
 
-        with pd.ExcelWriter(outputPathStudy + reference[:-1] + "_GS_Scores_Blending" + '_' + blendingScore + ".xlsx", mode='w') as writer:
+        with pd.ExcelWriter(outputPathStudy + reference[:-1] + "_GS_Scores_NBest" + '_' + str(NCount) + '_' + NBestScore + '_' + blendModel.GSName + ".xlsx", mode='w') as writer:
             for df, name in zip(AllDfs, sheetNames):
                 df.to_excel(writer, sheet_name=name)
 

SCRIPTS/StudyReport.py
@@ -140,11 +140,12 @@ def reportCV_Scores_NBest(studies_Blender, displayParams, DBpath, n, NBestScore,
             AllDfs.append(BlendingDf)
 
         for df in AllDfs:
-            slice = df.iloc[0:len(index)-1, :]
-            df.loc['Avg', :] = slice.mean(axis=0)
-            df.loc['Blender_Increase', :] = (df.loc['LR_RIDGE_Blender', :]/df.loc['Avg', :])-1
+            slice = df.iloc[0:len(df)-1, :]
+            df.loc['NBest_Avg', :] = slice.mean(axis=0)
+            df.loc['Blender_Increase', :] = (df.loc[studies_Blender[0].GSName, :]/df.loc['NBest_Avg', :])-1
 
-        with pd.ExcelWriter(outputPathStudy + reference[:-6] + "_CV_Scores_NBest" + '_' + str(n) + '_' + NBestScore + ".xlsx", mode='w') as writer:
+
+        with pd.ExcelWriter(outputPathStudy + reference[:-6] + "_CV_Scores_NBest" + '_' + str(n) + '_' + NBestScore + '_' + studies_Blender[0].GSName + ".xlsx", mode='w') as writer:
             for df, name in zip(AllDfs, sheetNames):
                 df.to_excel(writer, sheet_name=name)
 

SCRIPTS/StudyResiduals.py
@@ -118,12 +118,23 @@ def reportCV_ScoresAvg_All(studies, displayParams, DBpath):
     return ResultsDf
 
 
-def AssembleBlenderResiduals(studies_Blender):
+def AssembleNBestResiduals(studies_Blender): #todo : naming was changed
     residualsDict = dict()
     residualsDict["all"] = []
     for blender in studies_Blender:
         for model in blender.modelList:
             residualsDict["all"].append(list(model.Resid))
+
+    for k, v in residualsDict.items():
+        residualsDict[k] = mergeList(v)
+
+    return residualsDict
+
+def AssembleBlenderResiduals(studies_Blender):
+    residualsDict = dict()
+    residualsDict["all"] = []
+    for blender in studies_Blender:
+        residualsDict["all"].append(list(blender.Resid))
 
     for k, v in residualsDict.items():
         residualsDict[k] = mergeList(v)
@@ -252,7 +263,7 @@ def plotCVResidualsGaussian(studies, displayParams, FORMAT_Values, DBpath, study
 def plotCVResidualsHistogram_Combined(studies, displayParams, FORMAT_Values, DBpath, studyFolder ='Histplot', blended = False):
 
     if blended : #only takes nBestmodels
-        residualsDict = AssembleBlenderResiduals(studies)
+        residualsDict = AssembleNBestResiduals(studies)
         title = 'Residuals distribution for 10 best models over 10 runs'
     else : #takes all models
         residualsDict = AssembleCVResiduals(studies)
@@ -287,17 +298,23 @@ def plotCVResidualsHistogram_Combined(studies, displayParams, FORMAT_Values, DBp
 
 def plotCVResidualsGaussian_Combined(studies, displayParams, FORMAT_Values, DBpath, studyFolder='GaussianPlot',
                                      binwidth=25,
-                                     setxLim=[-300, 300], fontsize=14, blended = False):
+                                     setxLim=[-300, 300], fontsize=14, NBest = False, Blender = False):
     from scipy.stats import norm
     import seaborn as sns
 
     # assemble residuals
-    if blended : #only takes nBestmodels
-        residualsDict = AssembleBlenderResiduals(studies)
+    if NBest : #only takes nBestmodels
+        residualsDict = AssembleNBestResiduals(studies)
         title = 'Residuals distribution for 10 best models over 10 runs'
+        extra = '_'
+    elif Blender:  # only takes Blender results
+        residualsDict = AssembleBlenderResiduals(studies)
+        title = 'Residuals distribution for Blender Models over 10 runs ' + studies[0].GSName
+        extra = '_' + studies[0].GSName
     else : #takes all models
         residualsDict = AssembleCVResiduals(studies)
         title = 'Residuals distribution for all models over 10 runs'
+        extra = '_'
 
     listResVal = mergeList(list(residualsDict.values()))
     arr = np.array(listResVal)
@@ -355,7 +372,7 @@ def plotCVResidualsGaussian_Combined(studies, displayParams, FORMAT_Values, DBpa
         if not os.path.isdir(outputFigPath):
             os.makedirs(outputFigPath)
 
-        plt.savefig(outputFigPath + '/' + 'Combined' + '-' + studyFolder + '.png')
+        plt.savefig(outputFigPath + '/' + 'Combined' + '-' + studyFolder + extra + '.png')
 
     if displayParams['showPlot']:
         plt.show()
@@ -391,7 +408,9 @@ def RUN_CombinedResiduals(studies_GS_FS, studies_Blender, displayParams, FORMAT_
     reportCV_Residuals_All(models, means, variances, displayParams, DBpath)
 
     plotCVResidualsGaussian_Combined(studies_Blender, displayParams, FORMAT_Values, DBpath,
-                                     studyFolder='GaussianPlot_NBest_' + str(n) + '_' + NBestScore, blended=True)
+                                     studyFolder='GaussianPlot_NBest_' + str(n) + '_' + NBestScore, NBest=True)
+    plotCVResidualsGaussian_Combined(studies_Blender, displayParams, FORMAT_Values, DBpath,
+                                     studyFolder='GaussianPlot_Blender_' + str(n) + '_' + NBestScore, Blender=True)
     plotCVResidualsGaussian_Combined(studies_GS_FS, displayParams, FORMAT_Values, DBpath,
                                      studyFolder='GaussianPlot_groupedModels')
     plotCVResidualsGaussian(studies_GS_FS, displayParams, FORMAT_Values, DBpath,